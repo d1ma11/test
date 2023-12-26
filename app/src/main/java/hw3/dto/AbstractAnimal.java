b4b82d8e567b2f6435b2973a01cfadf424f0779f
Danilov Dmitriy <danilov0330@gmail.com>
2023-12-26 23:04:34 +0300

    HW-4: init commit

--- app/src/main/java/hw3/dto/AbstractAnimal.java
+++ app/src/main/java/hw3/dto/AbstractAnimal.java
@@ -1,10 +1,32 @@
 package hw3.dto;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
 
 public abstract class AbstractAnimal implements Animal {
     protected String breed; // порода
     protected String name; // имя
     protected BigDecimal cost; // цена
     protected String character; // характер
+    protected LocalDate birthDate; // день рождения
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof AbstractAnimal))
+            return false;
+        AbstractAnimal that = (AbstractAnimal) obj;
+        return breed.equals(that.breed) &&
+                name.equals(that.name) &&
+                cost.equals(that.cost) &&
+                character.equals(that.character) &&
+                birthDate.equals(that.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(breed, name, cost, character, birthDate);
+    }
 }
--- app/src/main/java/hw3/dto/Predator/Tiger.java
+++ app/src/main/java/hw3/dto/Predator/Tiger.java
@@ -2,17 +2,19 @@ package hw3.dto.Predator;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
 
 import hw3.dto.CharacterEnum;
 import hw3.dto.NamesEnum;
 
 public class Tiger extends Predator {
 
-    public Tiger(TigerBreeds breed, NamesEnum name, double cost, CharacterEnum character) {
+    public Tiger(TigerBreeds breed, NamesEnum name, double cost, CharacterEnum character, LocalDate birthDate) {
         this.breed = breed.toString().replace("_", " ");
         this.name = name.toString();
         this.cost = new BigDecimal(cost).setScale(2, RoundingMode.HALF_UP);
         this.character = character.toString();
+        this.birthDate = birthDate;
     }
 
     @Override
@@ -36,12 +38,17 @@ public class Tiger extends Predator {
     }
 
     @Override
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    @Override
     protected String hunt() {
         return "The " + this.getBreed().toLowerCase().replace("_", " ") + " starts hunting";
     }
 
     @Override
     public String toString() {
-        return "Tiger [breed=" + breed + ", name=" + name + ", cost=" + cost + ", character=" + character + "]";
+        return "Tiger [breed=" + breed + ", name=" + name + ", cost=" + cost + ", character=" + character + ", birthDate=" + birthDate + "]";
     }
 }
